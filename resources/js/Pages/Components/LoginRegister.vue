e8aaa22ea0347799dc54ca6b73fb4d25437b01f2
Kendy3-cmd <delcarmenkenneth0@gmail.com>
2025-02-25 19:46:19 +0800

    Changes

--- resources/js/Pages/Components/LoginRegister.vue
+++ resources/js/Pages/Components/LoginRegister.vue
@@ -5,6 +5,6 @@
 <template>
   <div class="container-Login-Register">
     <Link :href="route('EmployeeLoginRegister')" as="button" class="logRegEmployee">EMPLOYEE</Link>
-    <Link :href="route('MemberLoginRegister')" as="button" class="logRegMember">MEMBER</Link>
+    <Link :href="route('MemberLoginRegister')" as="button" class="logRegMember">MEMBERS</Link>
   </div>
 </template>
